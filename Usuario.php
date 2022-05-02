810214f81cb1e83e78f8b04b847d44605cca5413
Augusto <augustinhobrito542@gmail.com>
2022-05-02 15:45:37 -0300

    cria uma classe de usuario e divide o nome em duas strings

---
+++ Usuario.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Alura;
+
+class Usuario
+{
+
+    private $nome;
+    private $sobreNome;
+
+    public function __construct(string $nome)
+    {
+        $nomeSobrenome = explode(" ", $nome, 2);
+        $this->nome = $nomeSobrenome[0];
+        $this->sobreNome = $nomeSobrenome[1];
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function getSobrenome(): string
+    {
+        return $this->sobreNome;
+    }
+}
